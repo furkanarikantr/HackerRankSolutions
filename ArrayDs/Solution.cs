5c60fad5d90198cffd40082fccf76b37d26b5613
arikanfvs <furkanarikantr@gmail.com>
2023-10-28 21:48:15 +0300

    updated.

---
+++ ArrayDs/Solution.cs
@@ -0,0 +1,39 @@
+﻿namespace ArrayDs
+{
+    class Result
+    {
+        /*
+         * Complete the 'reverseArray' function below.
+         *
+         * The function is expected to return an INTEGER_ARRAY.
+         * The function accepts INTEGER_ARRAY a as parameter.
+         */
+
+        public static List<int> reverseArray(List<int> a)
+        {
+            a.Reverse();
+            return a;
+        }
+    }
+
+    internal class Solution
+    {
+        static void Main(string[] args)
+        {
+            //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+            int arrCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+            List<int> res = Result.reverseArray(arr);
+
+            string result = string.Join(" ", res);
+            Console.WriteLine(result);
+            //textWriter.WriteLine(String.Join(" ", res));
+
+            //textWriter.Flush();
+            //textWriter.Close();
+        }
+    }
+}
+
