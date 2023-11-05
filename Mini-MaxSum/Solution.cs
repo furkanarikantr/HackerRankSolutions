1f472a981b2dbcfa6a210b989f3cfec3c6bbc7ed
arikanfvs <furkanarikantr@gmail.com>
2023-11-05 18:32:48 +0300

    updated.

---
+++ Mini-MaxSum/Solution.cs
@@ -0,0 +1,54 @@
+﻿namespace Mini_MaxSum
+{
+    class Result
+    {
+        /*
+         * Complete the 'miniMaxSum' function below.
+         *
+         * The function accepts INTEGER_ARRAY arr as parameter.
+         */
+        public static void miniMaxSum(List<int> arr)
+        {
+            int temp = 0;
+            int minSum = 0;
+            int maxSum = 0;
+
+            //for (int i = 0; i < arr.Count(); i++)
+            //{
+            //    for (int j = i + 1; j < arr.Count(); j++)
+            //    {
+            //        if (arr[i] > arr[j])
+            //        {
+            //            temp = arr[i];
+            //            arr[i] = arr[j];
+            //            arr[j] = temp;
+            //        }
+            //    }
+            //}
+
+            arr.Sort((a, b) => a.CompareTo(b));
+
+            for (int i = 1; i < arr.Count(); i++)
+            {
+                maxSum += arr[i];
+            }
+
+            for (int i = 0; i < arr.Count() - 1; i++)
+            {
+                minSum += arr[i];
+            }
+
+            Console.WriteLine(minSum + " " + maxSum);
+        }
+    }
+
+    internal class Solution
+    {
+        static void Main(string[] args)
+        {
+            List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+            Result.miniMaxSum(arr);
+        }
+    }
+}
---
+++ PlusMinus/Solution.cs
@@ -0,0 +1,53 @@
+﻿namespace PlusMinus
+{
+    class Result
+    {
+        /*
+         * Complete the 'plusMinus' function below.
+         *
+         * The function accepts INTEGER_ARRAY arr as parameter.
+         */
+        public static void plusMinus(List<int> arr)
+        {
+            int numberOfPositive = 0;
+            int numberOfNegative = 0;
+            int numberOfEqualToZero = 0;
+
+            foreach (int item in arr)
+            {
+                if (item > 0)
+                {
+                    numberOfPositive += 1;
+                }
+                else if (item < 0)
+                {
+                    numberOfNegative += 1;
+                }
+                else
+                {
+                    numberOfEqualToZero += 1;
+                }
+            }
+
+            double ratioOfPositive = Convert.ToDouble(numberOfPositive) / Convert.ToDouble(arr.Count());
+            double ratioOfNMegative = Convert.ToDouble(numberOfNegative) / Convert.ToDouble(arr.Count());
+            double ratioOfZero = Convert.ToDouble(numberOfEqualToZero) / Convert.ToDouble(arr.Count());
+
+            Console.WriteLine(Math.Round(ratioOfPositive,6));
+            Console.WriteLine(Math.Round(ratioOfNMegative,6));
+            Console.WriteLine(Math.Round(ratioOfZero,6));
+        }
+    }
+
+    internal class Solution
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+            Result.plusMinus(arr);
+        }
+    }
+}
